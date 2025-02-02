17b58e0d7a83709aee966590d90422c4248757fb
AbdulRidhoRamadhan <abdulridhoramadhan.it@gmail.com>
2025-02-02 09:17:22 +0700

    Update API base URL to server address

--- admin/src/api/http-client.js
+++ admin/src/api/http-client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://cms-go-server.abdulridhoramadhan.my.id",
+  baseURL: "https://cms-go-server.ridhoramadhan.my.id",
 });
