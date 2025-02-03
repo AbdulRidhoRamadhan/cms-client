bec06cf33d73e8e771b1953393046aee2bc3dff1
AbdulRidhoRamadhan <abdulridhoramadhan.it@gmail.com>
2025-02-03 12:52:49 +0700

    Update item limit in Home component from 10 to 9

--- public/src/pages/Home.jsx
+++ public/src/pages/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
           search,
           sort,
           page,
-          limit: 10,
+          limit: 9,
           categoryName: category,
         },
       });
